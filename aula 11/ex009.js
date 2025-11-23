4a4f840e705ce5fe47b04d3a6b307aedd0c1d7ff
Alexandre Jemex Victorino Cavambe <245668036+alexandrecavambe@users.noreply.github.com>
2025-11-23 20:23:03 +0100

    Initial commit

---
+++ aula 11/ex009.js
@@ -0,0 +1,6 @@
+let vel = 30.5
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel > 60){//Condição simples
+    console.log("Você ultrapassou o limite de velocidade. MULTADO!")
+}
+console.log("Dirija seu carro com cuidado")
---
+++ aula 11/ex010.js
@@ -0,0 +1,7 @@
+let país = "Angola"
+console.log(`Vivendo em ${país}`)
+if(país != "Angola"){
+    console.log("Você é Estrangeiro")
+}else{
+    console.log("Você é Angolano")
+}
