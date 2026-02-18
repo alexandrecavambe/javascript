8fd569a2f349b9b555b7ba0b27b95753cbb43e9a
Alexandre Jemex Victorino Cavambe <245668036+alexandrecavambe@users.noreply.github.com>
2026-02-18 22:01:39 +0100

    commit

--- aula 12 ex/script.js
+++ aula 12 ex/script.js
@@ -8,16 +8,18 @@ let corpo = document.body
  function carregar(){
     let data = new Date()
     let hora = data.getHours()
-    hora = 18
     msg.innerHTML = `Agora são ${hora} horas`
     if(hora >= 0 && hora < 12){
         //Bom dia!
-        imagem.src = 'manhã.jpg'
+        imagem.src = 'imagem/manhã.jpg'
+        corpo.style.backgroundColor = '#ebdca5'
     }else if(hora >= 12 && hora < 18){
         //Boa tarde!
-        imagem.src = 'tarde.jpg'
+        imagem.src = 'imagem/tarde.jpg'
+        corpo.style.backgroundColor = '#817142'
     }else{
         //Boa noite!
-        imagem.src = 'noite.jpg'
+        imagem.src = 'imagem/noite.jpg'
+        corpo.style.backgroundColor = '#0B1C2D'
     }
 }
